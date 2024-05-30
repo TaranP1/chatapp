a913a6b4b4ba2fc89c92b5a4dc86ca6e235168d9
Taran <tapol2006@gmail.com>
2024-05-30 16:37:54 -0500

    first commit

---
+++ components/ChatInput.tsx
@@ -0,0 +1,56 @@
+"use client";
+import React from "react";
+import { Input } from "./ui/input";
+import { supabaseBrowser } from "@/lib/supabase/browser";
+import { v4 as uuidv4 } from "uuid";
+import { useUser } from "@/lib/store/user";
+import { Imessage, useMessage } from "@/lib/store/messages";
+
+export default function ChatInput() {
+	const user = useUser((state) => state.user);
+	const addMessage = useMessage((state) => state.addMessage);
+	const setOptimisticIds = useMessage((state) => state.setOptimisticIds);
+	const supabase = supabaseBrowser();
+	const handleSendMessage = async (text: string) => {
+		if (text.trim()) {
+			const id = uuidv4();
+			const newMessage = {
+				id,
+				text,
+				send_by: user?.id,
+				is_edit: false,
+				created_at: new Date().toISOString(),
+				users: {
+					id: user?.id,
+					avatar_url: user?.user_metadata.avatar_url,
+					created_at: new Date().toISOString(),
+					display_name: user?.user_metadata.user_name,
+				},
+			};
+			addMessage(newMessage as Imessage);
+			setOptimisticIds(newMessage.id);
+			const { error } = await supabase
+				.from("messages")
+				.insert({ text, id });
+			if (error) {
+				alert(error.message);
+			}
+		} else {
+			alert("Message can not be empty!!");
+		}
+	};
+
+	return (
+		<div className="p-5">
+			<Input
+				placeholder="send message"
+				onKeyDown={(e) => {
+					if (e.key === "Enter") {
+						handleSendMessage(e.currentTarget.value);
+						e.currentTarget.value = "";
+					}
+				}}
+			/>
+		</div>
+	);
+}
